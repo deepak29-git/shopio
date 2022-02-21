18db0b6267cc3c5d1059e92698ca04bf03c3a5ef
Deepak Goyal <dg887180@gmail.com>
2022-02-22 01:11:01 +0530

    refactor

--- Product-page/product-page.js
+++ Product-page/product-page.js
@@ -1,7 +1,6 @@
 const ecomHambergar=document.querySelector("#ecom-hamburger");
 const ecomSidebar=document.querySelector(".ecom-sidebar");
 
-// ecomSidebar.style.display="none";
 ecomHambergar.addEventListener("click",()=>{
     if(ecomSidebar.style.display==="block"){
         ecomSidebar.style.display="none"
